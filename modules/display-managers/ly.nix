effb69640ecd4ad8a24b794de247e79caa687e8c
speeif <speeif@fake-mail.com>
2026-05-16 07:29:55 +0200

    possibly fix ly commands

--- modules/display-managers/ly.nix
+++ modules/display-managers/ly.nix
@@ -41,53 +41,61 @@ in
           enable = true;
           package = pkgs.ly; # TUI -- zig -- https://codeberg.org/AnErrupTion/ly
           x11Support = true;
-          settings = {
+          settings = lib.mkMerge [
             # [config]
-            lang = "en";
-            service_name = "ly";
-            allow_empty_password = false;
-            clear_password = true; # on failure
-            auth_fails = 3; # display animation at X
-            default_input = "password";
-            inactive_command = null;
-
+            {
+              lang = "en";
+              service_name = "ly";
+              allow_empty_password = false;
+              clear_password = true; # on failure
+              auth_fails = 3; # display animation at X
+              default_input = "password";
+              inactive_command = null;
+            }
             # [information bars]
-            hide_key_hints = false;
-            hide_keyboard_lock = false;
-            hide_version_string = true;
+            {
+              hide_key_hints = false;
+              hide_keyboard_lock = false;
+              hide_version_string = true;
+            }
 
             # [animation]
-            animation = selectedAnimation;
-            animation_timeout_sec = 20; # run for X seconds
-            # [animation:colormix]
-            colormix_col1 = "0x00${theme.base09}"; # peach
-            colormix_col2 = "0x00${theme.base0C}"; # teal
-            colormix_col3 = "0x00${theme.base0E}"; # mauve
-
+            {
+              animation = selectedAnimation;
+              animation_timeout_sec = 20; # run for X seconds
+              # [animation:colormix]
+              colormix_col1 = "0x00${theme.base09}"; # peach
+              colormix_col2 = "0x00${theme.base0C}"; # teal
+              colormix_col3 = "0x00${theme.base0E}"; # mauve
+            }
             # [styling]
-            bg = "0x00${theme.base00}";
-            fg = "0x00${theme.base02}";
-            error_bg = "0x00000000";
-            error_fg = "0x00FF0000";
-
+            {
+              bg = "0x00${theme.base00}";
+              fg = "0x00${theme.base02}";
+              error_bg = "0x00000000";
+              error_fg = "0x00FF0000";
+            }
             # [box]
-            hide_borders = false;
-            blank_box = true;
-            border_fg = "0x00${theme.base08}";
-
+            {
+              hide_borders = false;
+              blank_box = true;
+              border_fg = "0x00${theme.base08}";
+            }
             # [functions]
-            restart_cmd = "/run/current-system/sw/bin/shutdown -r now";
-            restart_key = "F11";
-            shutdown_cmd = "/run/current-system/sw/bin/shutdown -h now";
-            shutdown_key = "F12";
-            sleep_cmd = null;
-            sleep_key = "F10";
+            {
+              restart_cmd = "${lib.getExe' pkgs.systemd "shutdown"} -r now";
+              restart_key = "F11";
+              shutdown_cmd = "${lib.getExe' pkgs.systemd "shutdown"} -h now";
+              shutdown_key = "F12";
+              sleep_cmd = null;
+              sleep_key = "F10";
 
-            brightness_down_key = "F2";
-            brightness_down_cmd = "${pkgs.brightnessctl}/bin/brightnessctl -q -n -s 10%-";
-            brightness_up_key = "F3";
-            brightness_up_cmd = "${pkgs.brightnessctl}/bin/brightnessctl -q -n -s 10%+";
-          };
+              brightness_down_key = "F2";
+              brightness_down_cmd = "${lib.getExe pkgs.brightnessctl} -q s 10%-";
+              brightness_up_key = "F3";
+              brightness_up_cmd = "${lib.getExe pkgs.brightnessctl} -q s 10%+";
+            }
+          ];
         };
       };
     };
