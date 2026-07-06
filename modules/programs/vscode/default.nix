2a02fc3ad5962cf6b48b77548925949ef4b95de5
speeif <speeif@fake-mail.com>
2026-07-06 11:57:15 +0200

    Added nix support for vscode (again (but now it finally works)), now using alejandra for formatting (prefers it)

--- modules/programs/vscode/default.nix
+++ modules/programs/vscode/default.nix
@@ -4,61 +4,43 @@
   self,
   self',
   ...
-}:
-let
+}: let
   moduleName = "vscode";
-in
-{
-  flake.homeModules."${moduleName}" =
-    { pkgs, config, ... }:
-    {
-      programs.vscodium = {
-        enable = true;
-        package = self.packages."${pkgs.stdenv.hostPlatform.system}"."${moduleName}";
-
-        profiles.default = {
-          enableUpdateCheck = false;
-          enableExtensionUpdateCheck = false;
-          userSettings.content = builtins.readFile ./settings.json;
-        };
-        # keybindings.source = ./keybinding.json;
-      };
+in {
+  #! NOTE: this is not how you do nixos modules
+  # Due to how I use vscode, trying out new extensions, in-app updatings, and generally
+  # my development flow depending on quick nudges as I am not 'set' in how I use this
+  #
+  # software yet, I want to limit the amount of changes I would need for this file.
+  # I have tried wrapping the module, I have tried using the command line tool to
+  # configure, and I have exhausted my 'wanting' to configure this piece fully.
+  #
+  # As this is also how 'vscode' was designed to be, I conscribe it to a fault
+  # in system philosophy, and may later try other code editing tools.
+  flake.homeModules."${moduleName}" = {
+    pkgs,
+    config,
+    ...
+  }: let
+    mkConfLink = target: config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${target}";
+  in {
+    home.packages = with pkgs; [
+      alejandra
+      nixd
+    ];
+    nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    xdg.configFile."VSCodium/User/keybindings.json".source = mkConfLink "/nix/flake/modules/programs/vscode/keybindings.json";
+    xdg.configFile."VSCodium/User/settings.json".source = mkConfLink "/nix/flake/modules/programs/vscode/settings.json";
+    programs.vscodium = {
+      enable = true;
+      package = pkgs.vscodium-fhs;
+      # keybindings.source = ./keybinding.json;
     };
-
-  perSystem =
-    {
-      pkgs,
-      config,
-      lib,
-      ...
-    }:
-    {
-      packages."${moduleName}" = inputs.wrappers.lib.wrapPackage {
-        inherit pkgs;
-        package =
-          with pkgs;
-          vscode-with-extensions.override {
-            vscode = vscodium;
-            #! Remember: `attribute 'vscodeExtUniqueId' missing` means that an extension was not found.
-            vscodeExtensions =
-              with vscode-extensions;
-              [
-                # Nix
-                bbenoist.nix
-                jnoortheen.nix-ide
-              ]
-              ++ [
-                # Themes   # Editor versatility and formatters
-                catppuccin.catppuccin-vsc
-                catppuccin.catppuccin-vsc-icons
-              ]
-              ++ [
-                # Editing extensions
-                esbenp.prettier-vscode
-                editorconfig.editorconfig
-                aaron-bond.better-comments
-              ];
-          };
-      };
+    programs.direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
     };
+  };
 }
